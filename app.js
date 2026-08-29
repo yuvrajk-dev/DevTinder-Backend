6230f514bc59cbc696f1fcd6588ccd4738ef08a4
Yuvraj Kumar <yuvrajk.dev@gmail.com>
2026-08-29 15:46:27 +0530

    feat: add CORS support to enable cross-origin requests

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ require("dotenv").config();
 const express = require("express");
 const connectDB = require("./src/config/database");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use(express.json());
 app.use(cookieParser());
