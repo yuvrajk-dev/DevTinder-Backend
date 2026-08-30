a0a74b6333f1f95304685ab5a88b1b30898b7897
Yuvraj Kumar <yuvrajk.dev@gmail.com>
2026-08-30 19:03:26 +0530

    feat: update CORS configuration to use environment variable for frontend URL

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const cors = require("cors");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({    origin: process.env.FRONTEND_URL,
+    credentials: true, }));
 
 app.use(express.json());
 app.use(cookieParser());
